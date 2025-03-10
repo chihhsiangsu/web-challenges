84838e2f36eac2e8189a01ae3fe33e8764b2922c
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-10 12:47:28 +0100

    statement done

--- js-condition-and-booleans/js-conditions-and-booleans_statements/js/index.js
+++ js-condition-and-booleans/js-conditions-and-booleans_statements/js/index.js
@@ -1,26 +1,49 @@
 console.clear();
 
 // Part 1: Password
-const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
+const SUPER_SECRET_PASSWORD = "h4x0r1337";
 const receivedPassword = "password1234";
+
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
 
 // Part 2: Even / Odd
 const number = 6;
 
+if (number % 2 === 0) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
+
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 999;
+
+if (numberOfHotdogs < 5) {
+  console.log("Price= " + "2" + "euros");
+} else if (5 < numberOfHotdogs < 100) {
+  console.log("Price= " + "1.5" + "euros");
+} else if (100 < numberOfHotdogs < 1000000) {
+  console.log("Price= " + "1" + "euro");
+} else if (numberOfHotdogs > 1000000) {
+  console.log("Price= " + "1" + "euro");
+}
 
 // Part 4: Daytime
 const currentHour = 12;
 
-const statement = "";
+let statement = "";
+
+statement = currentHour < 17 ? "Still need to learn..." : "Party time!";
 
 console.log(statement);
 
 // Part 5: Greeting
 const userName = "Archibald";
-
-const greeting = "Hello " + "//enter your code here" + "!";
+const greeting = "Hello " + (userName === "Chales" ? "Coach" : userName) + "!";
 
 console.log(greeting);
