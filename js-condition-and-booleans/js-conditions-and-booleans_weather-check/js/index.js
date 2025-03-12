a1198662d589dc495052d165edf2110d99838870
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-12 09:39:11 +0100

    update

--- js-condition-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
+++ js-condition-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,5 +1,20 @@
 // Change this value to test different "weather" conditions.
-const weather = "rainy"
+const weather = "sunny";
+
+switch (weather) {
+  case "sunny":
+    console.log("It's sunny!");
+    break;
+  case "rainy":
+    console.log("It's raining, don't forget your umbrella!");
+    break;
+  case "snowy":
+    console.log("It's snowing, stay warm!");
+    break;
+  default:
+    console.log("I'm not sure what the weather is like.");
+    break;
+}
 
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22; 
+const temperature = 22;
