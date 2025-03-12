c86251e13dd57f90edc4046e2c020943256d2bd7
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-12 20:57:02 +0100

    done

--- js-form/js-forms_calculator/js/index.js
+++ js-form/js-forms_calculator/js/index.js
@@ -25,7 +25,22 @@ form.addEventListener("submit", (event) => {
   let result;
 
   // --v-- write your code here --v--
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
 
+  const numberA = Number(data.numberA);
+  const numberB = Number(data.numberB);
+  const operator = data.operator;
+
+  if (operator === "addition") {
+    result = add(numberA, numberB);
+  } else if (operator === "subtraction") {
+    result = subtract(numberA, numberB);
+  } else if (operator === "multiplication") {
+    result = multiply(numberA, numberB);
+  } else if (operator === "division") {
+    result = divide(numberA, numberB);
+  }
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
