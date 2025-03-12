d096f6e9ab5824ab6f8bea93c97125679251148b
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-12 13:57:45 +0100

    done

--- js-input-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-input-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,21 @@
 console.clear();
+const inputColor = document.querySelector('[data-js="input-color"]');
+const inputRadius = document.querySelector('[data-js="input-radius"]');
+const inputRotation = document.querySelector('[data-js="input-rotation"]');
+const box = document.querySelector('[data-js="box"]');
+
+inputColor.addEventListener("input", () => {
+  const hue = inputColor.value;
+  box.style.backgroundColor = `hsl(${hue},50%,50%)`;
+});
+
+inputRadius.addEventListener("input", () => {
+  box.style.borderRadius =
+    Number(inputRadius.value) === Number(inputRadius.max)
+      ? "100%"
+      : `${inputRadius.value}px`;
+});
+
+inputRotation.addEventListener("input", () => {
+  box.style.transform = `rotate(${inputRotation.value}deg)`;
+});
