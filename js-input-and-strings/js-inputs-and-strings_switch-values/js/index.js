53a4fc49308fb4989c6368ebbedcb9a21118cf0a
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-12 11:45:11 +0100

    switch

--- js-input-and-strings/js-inputs-and-strings_switch-values/js/index.js
+++ js-input-and-strings/js-inputs-and-strings_switch-values/js/index.js
@@ -1,8 +1,12 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchValueButton = document.querySelector(
+  '[data-js="button-switch-value"]'
+);
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
@@ -10,4 +14,8 @@ uppercaseButton.addEventListener("click", () => {
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+});
+
+switchValueButton.addEventListener("click", () => {
+  [firstInput.value, secondInput.value] = [secondInput.value, firstInput.value];
 });
