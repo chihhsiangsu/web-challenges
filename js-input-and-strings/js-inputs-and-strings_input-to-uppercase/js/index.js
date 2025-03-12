fd7bc47bdb243f47bfd299053c3a69b5a0602e06
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-12 11:08:32 +0100

    uppercase done

--- js-input-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-input-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,10 @@
 console.clear();
+
+const firstInput = document.querySelector('[data-js="first-input"]');
+const upperButton = document.querySelector('[data-js="button-uppercase"]');
+
+upperButton.addEventListener("click", () => {
+  const oldValue = firstInput.value;
+  const newValue = oldValue.toUpperCase();
+  firstInput.value = newValue;
+});
