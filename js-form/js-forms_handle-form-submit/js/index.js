11e48401e7dec22f5afc180fd7ba72702763292a
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-12 20:15:14 +0100

    update

--- js-form/js-forms_handle-form-submit/js/index.js
+++ js-form/js-forms_handle-form-submit/js/index.js
@@ -8,7 +8,15 @@ form.addEventListener("submit", (event) => {
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
-  console.log(data);
+  console.log(data.firstName);
+  console.log(data.lastName);
+  console.log(data.age);
+  console.log(data.email);
+  console.log(data.complaint);
+  console.log(data.details);
+  console.log(data.badness);
+  console.log(data.orderDate);
+  console.log(data.tos);
 
   event.target.elements.firstName.focus();
   event.target.reset();
