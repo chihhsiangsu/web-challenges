1044fd2a0829ab60cf2b32d7fc4b74e5bc25c13e
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-13 11:37:06 +0100

    done

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,18 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const newMessage = document.createElement("li");
+  newMessage.textContent = "new message";
+  newMessage.classList.add("toast-container__message");
+
+  toastContainer.append(newMessage);
 });
 
-clearButton.addEventListener("click", () => {
+clearButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  toastContainer.textContent = "";
   // Exercise: Clear the stack of toast messages
 });
