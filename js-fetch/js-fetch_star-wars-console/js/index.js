fbe61f588b2ba0356ac3d22092261ec90763cd30
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-19 11:13:24 +0100

    done

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,21 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  //console.log(response);
 
+  const data = await response.json();
+  //console.log(data.results);
+  return data.results;
+}
+async function findR2d2Eyecolor() {
+  const allData = await fetchData();
+  const r2d2 = allData.find((feacher) => feacher.name === "R2-D2");
+  if (r2d2) {
+    console.log(r2d2.eye_color);
+  }
+}
+
+findR2d2Eyecolor();
 fetchData();
