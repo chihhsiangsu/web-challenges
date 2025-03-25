ba12824bd4cfe6295043c1dd8b1a2244fd17a991
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-25 16:03:11 +0100

    second react done

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,14 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return (
+    <article className="article">
+      <h2 className="article__title">Hello</h2>
+      <label htmlFor="input">Type something</label>
+      <input id="input"></input>
+      <a className="article__link" href="https://en.wikipedia.org/wiki/Hello">
+        What is Hello meaning?
+      </a>
+    </article>
+  );
 }
