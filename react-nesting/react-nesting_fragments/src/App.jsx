c9a9bfa1e4b1e4106ef9fa4641639063bc900560
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-26 15:38:31 +0100

    Fragment done

--- react-nesting/react-nesting_fragments/src/App.jsx
+++ react-nesting/react-nesting_fragments/src/App.jsx
@@ -4,10 +4,18 @@ import Box from "./components/Box";
 
 export default function App() {
   return (
-    <main className="flex-container">
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
-    </main>
+    <mian className="flex-container">
+      <Boxes color="#007bff" />
+      <Boxes color="#fc3" />
+      <Boxes color="#ff3333" />
+    </mian>
+  );
+}
+
+function Boxes({ color }) {
+  return (
+    <>
+      <div className="box" style={{ backgroundColor: color }}></div>
+    </>
   );
 }
