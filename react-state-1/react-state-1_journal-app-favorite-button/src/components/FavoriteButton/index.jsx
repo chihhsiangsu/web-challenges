9f1dc331afa02c7d5f578e01a471b93efeb967f8
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-28 12:01:12 +0100

    bookmark done

--- react-state-1/react-state-1_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
+++ react-state-1/react-state-1_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
@@ -1,16 +1,24 @@
 import "./FavoriteButton.css";
 import StarFilled from "./star-filled.svg?react";
 import Star from "./star.svg?react";
+import { useState } from "react";
 
 export default function FavoriteButton() {
   // This should be a state variable.
-  const isFavorite = false;
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  function handdleToggle() {
+    setIsFavorite(!isFavorite);
+  }
 
   return (
     <button
-      className="favorite-button"
+      className={
+        isFavorite ? "favorite-button:hover " : "favorite-button:active"
+      }
       onClick={() => {
-        console.log("favorite button clicked");
+        handdleToggle();
+        //console.log("favorite button clicked");
       }}
       aria-label="favorite"
     >
