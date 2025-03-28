fd03d6387929dfe4a8cdf450de254ec88e1a73f6
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-03-28 15:44:49 +0100

    updates

--- react-with-array/react-with-arrays_users/src/App.jsx
+++ react-with-array/react-with-arrays_users/src/App.jsx
@@ -7,10 +7,14 @@ import { USERS } from "./db";
 export default function App() {
   return (
     <main className="app">
-      <Title text="ClientBoard" />
-      <div className="app__card-grid">
-        <Card user={USERS[0]} />
-      </div>
+      {USERS.map((user) => (
+        <Fragment>
+          <Title text="ClientBoard" />
+          <div className="app__card-grid">
+            <Card user={USERS[0]} />
+          </div>
+        </Fragment>
+      ))}
     </main>
   );
 }
