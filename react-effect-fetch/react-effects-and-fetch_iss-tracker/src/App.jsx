57eb897a1676829a55d7ee91ffd74286ee92a213
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-04-01 12:07:30 +0200

    iss-traker done

--- react-effect-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
+++ react-effect-fetch/react-effects-and-fetch_iss-tracker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
@@ -11,7 +11,23 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    const response = await fetch(URL);
+    const coords = await response.json();
+    // to get the coords
+    console.log(coords);
+    setCoords({ longitude: coords.longitude, latitude: coords.latitude });
+  }
+
+  // setInterval return the intervalID ->The clearInterval() method of the Window interface
+  // cancels a timed, repeating action which was previously established by a call to
+  // setInterval(). If the parameter provided does not identify a previously
+  // established action, this method does nothing.
+
+  useEffect(() => {
+    const intervalID = setInterval(getISSCoords, 5 * 1000);
+    return () => clearInterval(intervalID);
+  }, []);
 
   return (
     <main>
