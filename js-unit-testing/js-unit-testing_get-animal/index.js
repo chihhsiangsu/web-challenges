75fbe21d6267270802df0e216ee49c5654fe0fc4
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-04-09 15:40:25 +0200

    done

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animals) {
+  if (animals === "cats") {
+    return `I totally love cats!`;
+  } else if (!animals) {
+    return `I do not like animals at all!`;
+  } else {
+    return `I like ${animals}!`;
+  }
+}
