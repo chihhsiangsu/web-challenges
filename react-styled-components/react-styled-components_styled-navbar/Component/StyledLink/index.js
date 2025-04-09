f507ae833972af6d53e6e75ccea3842aae0c7281
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-04-09 11:51:42 +0200

    StyledLink

---
+++ react-styled-components/react-styled-components_styled-navbar/Component/StyledLink/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  &:hover {
+    text-decoration: green wavy underline;
+  }
+`;
