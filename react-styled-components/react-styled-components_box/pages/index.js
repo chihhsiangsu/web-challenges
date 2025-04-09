a22a0c38ac12e4864468cecbc42251c0f8a8ac4b
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-04-09 12:17:52 +0200

    styled-box

--- react-styled-components/react-styled-components_box/pages/index.js
+++ react-styled-components/react-styled-components_box/pages/index.js
@@ -1,10 +1,10 @@
-import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import { BoxWithStyledComponents } from "@/components/BoxWithStyledComponents/index.js";
 
 export default function HomePage() {
   return (
     <div>
-      <BoxWithClassName />
-      <BoxWithClassName isBlack />
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents $isBlack="isBlack" />
     </div>
   );
 }
