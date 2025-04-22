85e02a8ce55d6e1761d142152025fc11b487c217
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-04-22 14:50:41 +0200

    done

--- backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,18 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, isLoading } = useSWR(`/api/random-character`, fetcher);
+
+  if (isLoading) {
+    return <>Data is loading..</>;
+  }
+
+  return (
+    <h1>
+      {data.firstName}
+      {data.lastName}
+    </h1>
+  );
 }
