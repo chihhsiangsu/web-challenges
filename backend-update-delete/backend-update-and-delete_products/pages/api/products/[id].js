b81b4f939dc9ecaa448132eadaba434d049110ea
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-04-24 11:42:13 +0200

    done

--- backend-update-delete/backend-update-and-delete_products/pages/api/products/[id].js
+++ backend-update-delete/backend-update-and-delete_products/pages/api/products/[id].js
@@ -17,5 +17,18 @@ export default async function handler(request, response) {
     return;
   }
 
+  if (request.method === "PUT") {
+    const updateProduct = request.body;
+    await Product.findByIdAndUpdate(id, updateProduct);
+    response.status(200).json({ status: `Product ${id} is updated!` });
+    return;
+  }
+
+  if (request.method === "DELETE") {
+    await Product.findByIdAndDelete(id);
+    response.status(200).json({ status: `Product ${id} is deleted!` });
+    return;
+  }
+
   response.status(405).json({ status: "Method not allowed." });
 }
